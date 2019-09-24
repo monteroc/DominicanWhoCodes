a18a27f8f06496e7418f2661f3e0531fa271f368
Ariel De Los Santos <ariel@monteroc.com>
2019-09-23 22:57:30 -0400

    Refit configuration

---
+++ DominicanWhoCodes/Keys/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DominicanWhoCodes.Keys
+{
+    public static class Constants
+    {
+        public static string BaseApiUrl = "http://www.dominicanwhocodes.somee.com/api";
+        public static string BaseUrl = "http://www.dominicanwho.codes";
+    }
+}
+
---
+++ DominicanWhoCodes/Models/Developer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DominicanWhoCodes.Models
+{
+    public class Developer
+    {
+        public string Name { get; set; }
+        public string Initials { get; set; }
+        public string Image { get; set; }
+        public string Summary { get; set; }
+        public string Skills { get; set; }
+        public string Webpage { get; set; }
+        public string LinkedIn { get; set; }
+        public string Twitter { get; set; }
+        public string Github { get; set; }
+    }
+}
